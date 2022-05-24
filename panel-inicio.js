4d24506bb63379772ad9dee74e5271f16eed1604
Valentino Giannico <valentinogiannico4@gmail.com>
2022-05-24 01:31:59 -0300

    nuevos cambios

--- panel-inicio.js
+++ panel-inicio.js
@@ -6,3 +6,23 @@ hamburger.addEventListener("click", (e) => {
     hamburger.classList.toggle("block")
 })
 
+/* BUSCADOR */
+
+document.addEventListener("keyup", (e) => {
+    if(e.target.matches("#buscador")){
+
+        document.querySelectorAll(".opcion-buscador").forEach(opcion =>{
+
+            opcion.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?opcion.classList.add("oculto")
+            :opcion.classList.remove("oculto")
+
+
+            if(document.getElementById("buscador").value.length === 0){
+                opcion.classList.remove("oculto")
+            }
+        })
+
+
+    }
+})
