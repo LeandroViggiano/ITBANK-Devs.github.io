3db66cb9cf9558617ef4a09f368aafdfa86cf085
Valentino Giannico <valentinogiannico4@gmail.com>
2022-05-25 13:44:48 -0300

    cambios

--- panel-inicio.js
+++ panel-inicio.js
@@ -6,3 +6,23 @@ hamburger.addEventListener("click", (e) => {
     hamburger.classList.toggle("block")
 })
 
+/* CAROUSEL */
+const imagen = document.getElementById("imagen1");
+
+document.addEventListener("click", (e) => {
+    if(e.target.matches("#izquierda")){
+        if(imagen.getAttribute("src") === "¿TODAVIA NO PEDISTE LA NUEVA TARJETA ITABANK GOLD.png"){
+            imagen.setAttribute("src", "banner2.png")
+        }else if(imagen.getAttribute("src") === "banner2.png"){
+            imagen.setAttribute("src", "¿TODAVIA NO PEDISTE LA NUEVA TARJETA ITABANK GOLD.png")
+        }
+    }
+    
+    if(e.target.matches("#derecha")){
+        if(imagen.getAttribute("src") === "¿TODAVIA NO PEDISTE LA NUEVA TARJETA ITABANK GOLD.png"){
+            imagen.setAttribute("src", "banner2.png")
+        }else if(imagen.getAttribute("src") === "banner2.png"){
+            imagen.setAttribute("src", "¿TODAVIA NO PEDISTE LA NUEVA TARJETA ITABANK GOLD.png")
+        }
+    }
+})
