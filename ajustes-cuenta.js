e3dff421b80fb96fb54d5cb8825c02f26ee6bb06
Leandro Viggiano <leandrom.v@outlook.com>
2022-06-19 18:14:13 -0300

    eliminacion y creacion de rama + cambio de colores

--- ajustes-cuenta.js
+++ ajustes-cuenta.js
@@ -1,59 +1,207 @@
-/*Esto se encarga de que al hacer click en una opcion, se desplieguen mas opciones */
-let hideText_btn = document.getElementById("Tusdatos");
-let hideText = document.getElementById("hideTusdatos");
+let Tusdatos = document.getElementById("Tusdatos");
+let hideTusdatos = document.getElementById("hideTusdatos");
 
-hideText_btn.addEventListener("click", function (event) {
+/* MENU HAMBURGUESA */
+const hamburger = document.getElementById("menu");
+const panel = document.querySelector(".izquierda");
+
+hamburger.addEventListener("click", (e) => {
+    panel.classList.toggle("block")
+    hamburger.classList.toggle("block")
+})
+
+/* APP MOBIL */
+
+if(navigator.userAgent.match(/android/i)){
+    const alerta = document.querySelector(".mensaje-app").style.setProperty("display", "block")
+    const mensaje = document.querySelector(".app").textContent= "¡Descargá nuestra app en la Play Store!"
+} 
+
+if(navigator.userAgent.match(/iphone | ipad/i)) {
+    const alerta = document.querySelector(".mensaje-app").style.setProperty("display", "block")
+    const mensaje = document.querySelector(".app").textContent= "¡Descargá nuestra app en la App Store!"
+}
+
+Tusdatos.addEventListener("click", function (event) {
     event.preventDefault(); /*Evita acciones por default de las etiquetas */
-    toggleText(hideText)
+    toggleText(hideTusdatos)/*Esto se encarga de que al hacer click en una opcion, se desplieguen mas opciones */
+    if(hideComunicaciones.classList.contains("show")){
+        toggleText(hideComunicaciones);
+    }
+    if(hideSubscripciones.classList.contains("show")){
+        toggleText(hideSubscripciones);
+    }
+    if(hidePrivacidad.classList.contains("show")){
+        toggleText(hidePrivacidad);
+    }
+    if(hideTustarjetas.classList.contains("show")){
+        toggleText(hideTustarjetas);
+    }
+    if(hideSeguridad.classList.contains("show")){
+        toggleText(hideSeguridad);
+    }
+    if(hideDirecciones.classList.contains("show")){
+        toggleText(hideDirecciones);
+    }
 });
 /*------------------------------------------------- */
-let hideText_btn1 = document.getElementById("Seguridad");
-let hideText1 = document.getElementById("hideSeguridad");
+let Seguridad = document.getElementById("Seguridad");
+let hideSeguridad = document.getElementById("hideSeguridad");
 
-hideText_btn1.addEventListener("click", function (event) {
+Seguridad.addEventListener("click", function (event) {
     event.preventDefault();
-    toggleText(hideText1)
+    toggleText(hideSeguridad);
+    if(hideComunicaciones.classList.contains("show")){
+        toggleText(hideComunicaciones);
+    }
+    if(hideSubscripciones.classList.contains("show")){
+        toggleText(hideSubscripciones);
+    }
+    if(hidePrivacidad.classList.contains("show")){
+        toggleText(hidePrivacidad);
+    }
+    if(hideTustarjetas.classList.contains("show")){
+        toggleText(hideTustarjetas);
+    }
+    if(hideDirecciones.classList.contains("show")){
+        toggleText(hideDirecciones);
+    }
+    if(hideTusdatos.classList.contains("show")){
+        toggleText(hideTusdatos);
+    }
 }); 
 /*------------------------------------------------- */
-let hideText_btn2 = document.getElementById("Tustarjetas");
-let hideText2 = document.getElementById("hideTustarjetas");
+let Tustarjetas = document.getElementById("Tustarjetas");
+let hideTustarjetas = document.getElementById("hideTustarjetas");
 
-hideText_btn2.addEventListener("click", function (event) {
+Tustarjetas.addEventListener("click", function (event) {
     event.preventDefault();
-    toggleText(hideText2)
+    toggleText(hideTustarjetas);
+    if(hideComunicaciones.classList.contains("show")){
+        toggleText(hideComunicaciones);
+    }
+    if(hideSubscripciones.classList.contains("show")){
+        toggleText(hideSubscripciones);
+    }
+    if(hidePrivacidad.classList.contains("show")){
+        toggleText(hidePrivacidad);
+    }
+    if(hideDirecciones.classList.contains("show")){
+        toggleText(hideDirecciones);
+    }
+    if(hideSeguridad.classList.contains("show")){
+        toggleText(hideSeguridad);
+    }
+    if(hideTusdatos.classList.contains("show")){
+        toggleText(hideTusdatos);
+    }
 }); 
 /*------------------------------------------------- */
-let hideText_btn3 = document.getElementById("Direcciones");
-let hideText3 = document.getElementById("hideDirecciones");
+let Direcciones = document.getElementById("Direcciones");
+let hideDirecciones = document.getElementById("hideDirecciones");
 
-hideText_btn3.addEventListener("click", function (event) {
+Direcciones.addEventListener("click", function (event) {
    event.preventDefault();
-   toggleText(hideText3)
+   toggleText(hideDirecciones);
+   if(hideComunicaciones.classList.contains("show")){
+    toggleText(hideComunicaciones);
+}
+if(hideSubscripciones.classList.contains("show")){
+    toggleText(hideSubscripciones);
+}
+if(hidePrivacidad.classList.contains("show")){
+    toggleText(hidePrivacidad);
+}
+if(hideTustarjetas.classList.contains("show")){
+    toggleText(hideTustarjetas);
+}
+if(hideSeguridad.classList.contains("show")){
+    toggleText(hideSeguridad);
+}
+if(hideTusdatos.classList.contains("show")){
+    toggleText(hideTusdatos);
+}
 }); 
 /*------------------------------------------------- */
-let hideText_btn4 = document.getElementById("Privacidad");
-let hideText4 = document.getElementById("hidePrivacidad");
+let Privacidad = document.getElementById("Privacidad");
+let hidePrivacidad = document.getElementById("hidePrivacidad");
 
-hideText_btn4.addEventListener("click", function (event) {
+Privacidad.addEventListener("click", function (event) {
     event.preventDefault();
-    toggleText(hideText4)
+    toggleText(hidePrivacidad);
+    if(hideComunicaciones.classList.contains("show")){
+        toggleText(hideComunicaciones);
+    }
+    if(hideSubscripciones.classList.contains("show")){
+        toggleText(hideSubscripciones);
+    }
+    if(hideDirecciones.classList.contains("show")){
+        toggleText(hideDirecciones);
+    }
+    if(hideTustarjetas.classList.contains("show")){
+        toggleText(hideTustarjetas);
+    }
+    if(hideSeguridad.classList.contains("show")){
+        toggleText(hideSeguridad);
+    }
+    if(hideTusdatos.classList.contains("show")){
+        toggleText(hideTusdatos);
+    }
+
 }); 
 
 /*------------------------------------------------- */
-let hideText_btn5 = document.getElementById("Comunicaciones");
-let hideText5 = document.getElementById("hideComunicaciones");
+let Comunicaciones = document.getElementById("Comunicaciones");
+let hideComunicaciones = document.getElementById("hideComunicaciones");
 
-hideText_btn5.addEventListener("click", function (event) {
+Comunicaciones.addEventListener("click", function (event) {
     event.preventDefault();
-    toggleText(hideText5)
+    toggleText(hideComunicaciones);
+    if(hideSubscripciones.classList.contains("show")){
+        toggleText(hideSubscripciones);
+    }
+    if(hidePrivacidad.classList.contains("show")){
+        toggleText(hidePrivacidad);
+    }
+    if(hideDirecciones.classList.contains("show")){
+        toggleText(hideDirecciones);
+    }
+    if(hideTustarjetas.classList.contains("show")){
+        toggleText(hideTustarjetas);
+    }
+    if(hideSeguridad.classList.contains("show")){
+        toggleText(hideSeguridad);
+    }
+    if(hideTusdatos.classList.contains("show")){
+        toggleText(hideTusdatos);
+    }
+
 }); 
 /*------------------------------------------------- */
-let hideText_btn6 = document.getElementById("Subscripciones");
-let hideText6 = document.getElementById("hideSubscripciones");
+let Subscripciones = document.getElementById("Subscripciones");
+let hideSubscripciones = document.getElementById("hideSubscripciones");
 
-hideText_btn6.addEventListener("click", function (event) {
+Subscripciones.addEventListener("click", function (event) {
     event.preventDefault();
-    toggleText(hideText6)
+    toggleText(hideSubscripciones);
+    if(hideComunicaciones.classList.contains("show")){
+        toggleText(hideComunicaciones);
+    }
+    if(hidePrivacidad.classList.contains("show")){
+        toggleText(hidePrivacidad);
+    }
+    if(hideDirecciones.classList.contains("show")){
+        toggleText(hideDirecciones);
+    }
+    if(hideTustarjetas.classList.contains("show")){
+        toggleText(hideTustarjetas);
+    }
+    if(hideSeguridad.classList.contains("show")){
+        toggleText(hideSeguridad);
+    }
+    if(hideTusdatos.classList.contains("show")){
+        toggleText(hideTusdatos);
+    }
 }); 
 /*------------------------------------------------- */
 function toggleText(element) {
