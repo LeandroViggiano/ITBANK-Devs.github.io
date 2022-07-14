46d791bb16989bd55c48cfaafa5f7e0fd0b58105
Valentino Giannico <valentinogiannico4@gmail.com>
2022-07-14 19:24:44 -0300

    nuevos cambios

--- Base.py
+++ Base.py
@@ -8,7 +8,6 @@ with open('eventos_classic.json' , 'r') as archivo:
 
 
 # PARTE CLIENTE DEL UML
-#Creando Constructor
 class Cliente:
     def __init__(Clientes, nombre, apellido, numero, dni, tipo):
         Cliente.nombre = nombre
@@ -16,24 +15,23 @@ class Cliente:
         Cliente.numero = numero
         Cliente.dni = dni
         Cliente.tipo = tipo
-#Constructor terminado y creado
+        Cliente.direccion = (datos_del_cliente['direccion']['calle'],datos_del_cliente['direccion']['numero'],datos_del_cliente['direccion']['ciudad'],datos_del_cliente['direccion']['provincia'],datos_del_cliente['direccion']['pais'])
 
 #Creo los metodos
     def puede_crear_chequera(Clientes):
         pass
     def puede_crear_tarjeta_credito(Clientes):
-        Cliente._aceptado = True
+        pass
     def puede_comprar_dolar(Clientes):
-        Cliente._aceptado = True
+        pass
 #Metodos creados 
 
     def __str__(Cliente): #Al mostrar un objeto, se muestra feo, esta linea de codigo hace que se vea más bonito
         return 'Nombre ' + str(Cliente.nombre) + ', apellido: ' + (Cliente.apellido) + ', numero: ' + str(Cliente.numero) + ', dni: ' + str(Cliente.dni) + ', tipo: ' + str(Cliente.tipo)
 #Acá creo un objeto cliente llamado c1 y le agrego los atributos de la lista datos_del_cliente
 cliente1 = Cliente(datos_del_cliente['nombre'], datos_del_cliente['apellido'], datos_del_cliente['numero'], datos_del_cliente['dni'], datos_del_cliente['tipo'])
-#print(cliente1)
-#PARTE CLASSIC GOLD Y BLACK
 
+#PARTE CLASSIC GOLD Y BLACK
 
 if Cliente.tipo == 'CLASSIC':    
     class Classic (Cliente):
@@ -197,12 +195,4 @@ class Cuenta:
         Cuentas.costo_transferencias = costo_transferencias
         Cuentas.saldo_descubierto_disponible = saldo_descubierto_disponible
 #cuenta1 = Cuenta(datos_del_cliente['nombre'])
-# PARTE CUENTA DEL UML
-#Creando Constructor
-class Direccion:
-    def __init__(Cuentas,calle,numero,ciudad,provincia,pais):
-        Direccion.calle=calle
-        Direccion.numero=numero
-        Direccion.ciudad=ciudad
-        Direccion.provincia=provincia
-        Direccion.pais=pais
+
